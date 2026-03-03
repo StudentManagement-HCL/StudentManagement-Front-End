8d34b159df22ae1b9505b2fd7da630c7355f1729
717822f214@kce.ac.in <dhanushmms007@gmail.com>
2026-03-03 11:04:07 +0530

    Updated admin dashboard, integrated backend APIs, fixed port configuration

---
+++ src/app/services/admin.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+private apiUrl = 'http://localhost:5035/api/Student';
+
+getStudents() {
+  return this.http.get(`${this.apiUrl}/GetStudents`);
+}
+
+AddStudent(student: any) {
+  return this.http.post(`${this.apiUrl}/AddStudent`, student);
+}
+
+updateStudent(id: number, student: any) {
+  return this.http.put(`${this.apiUrl}/EditStudent/${id}`, student);
+}
+
+deleteStudent(id: number) {
+  return this.http.delete(`${this.apiUrl}/DeleteStudent/${id}`);
+}
+
+}
